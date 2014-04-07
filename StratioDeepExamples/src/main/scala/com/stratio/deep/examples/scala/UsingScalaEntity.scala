e72726e001c5885052004a3c342bec2ae0f8d5f4
Luca Rosellini <luca@stratio.com>
2014-04-07 12:32:43 +0200

    Added license header to StratioDeepExamples source files

--- StratioDeepExamples/src/main/scala/com/stratio/deep/examples/scala/UsingScalaEntity.scala
+++ StratioDeepExamples/src/main/scala/com/stratio/deep/examples/scala/UsingScalaEntity.scala
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.stratio.deep.examples.scala
 
 import com.stratio.deep.context.DeepSparkContext
