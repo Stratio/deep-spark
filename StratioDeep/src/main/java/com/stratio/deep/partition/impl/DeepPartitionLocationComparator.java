1e94104b21d84cf84680fe6a185f599f52bb549c
Luca Rosellini <luca@stratio.com>
2014-04-09 15:08:07 +0200

    Added partition comparator that will appropriately reorder the list of locations for each split

---
+++ StratioDeep/src/main/java/com/stratio/deep/partition/impl/DeepPartitionLocationComparator.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2014, Stratio.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.stratio.deep.partition.impl;
+
+import com.stratio.deep.exception.DeepInstantiationException;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.util.Comparator;
+
+/**
+ * Created by luca on 09/04/14.
+ */
+public class DeepPartitionLocationComparator implements Comparator<String> {
+    private InetAddress hostname;
+
+    public DeepPartitionLocationComparator(){
+        try {
+            this.hostname = InetAddress.getByName(System.getenv("HOSTNAME"));
+        } catch (UnknownHostException e) {
+            throw new DeepInstantiationException(e);
+        }
+    }
+
+    public DeepPartitionLocationComparator(String hostname) {
+        try {
+            this.hostname = InetAddress.getByName(hostname);
+        } catch (UnknownHostException e) {
+            throw new DeepInstantiationException(e);
+        }
+    }
+
+    private Integer getIp(InetAddress addr) {
+        byte[] a = addr.getAddress();
+        return ((a[0] & 0xff) << 24) | ((a[1] & 0xff) << 16) | ((a[2] & 0xff) << 8) | (a[3] & 0xff);
+    }
+
+    @Override
+    public int compare(String loc1, String loc2) {
+        int result = 0;
+
+        try {
+            InetAddress addr1 = InetAddress.getByName(loc1);
+            InetAddress addr2 = InetAddress.getByName(loc2);
+
+            if (addr1 == addr2) {
+                result = 0;
+            } else if (addr1.isLoopbackAddress()){
+                result = -1;
+            } else if (addr2.isLoopbackAddress()){
+                result = 1;
+            } else if (addr1.getHostAddress().equals(hostname.getHostAddress())){
+                result = -1;
+            } else if (addr2.getHostAddress().equals(hostname.getHostAddress())){
+                result = 1;
+            }
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+}
