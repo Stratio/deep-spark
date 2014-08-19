1c953af0e075104d6fdcfc2e0a07fee4757fe8dc
David Gomez <dgomezperez@gmail.com>
2014-08-19 13:24:09 +0200

    git commit  deep-commons/src/main/java/com/stratio/deep/entity/Cell.java 	git commit  deep-commons/src/main/java/com/stratio/deep/entity/Cells.java 	git commit  deep-commons/src/main/java/com/stratio/deep/extractor/actions/Action.java 	git commit  deep-commons/src/main/java/com/stratio/deep/extractor/actions/ActionType.java 	git commit  deep-commons/src/main/java/com/stratio/deep/extractor/actions/ComputeAction.java 	git commit  deep-commons/src/main/java/com/stratio/deep/extractor/actions/GetPartitionsAction.java 	git commit  deep-commons/src/main/java/com/stratio/deep/extractor/actions/TransformElementAction.java 	git commit  deep-commons/src/main/java/com/stratio/deep/extractor/client/ExtractorClient.java 	git commit  deep-commons/src/main/java/com/stratio/deep/extractor/client/ExtractorClientHandler.java 	git commit  deep-commons/src/main/java/com/stratio/deep/extractor/client/ExtractorClientInitializer.java 	git commit  deep-commons/src/main/java/com/stratio/deep/extractor/client/codecs/ActionDecoder.java 	git commit  deep-commons/src/main/java/com/stratio/deep/extractor/client/codecs/ActionEncoder.java 	git commit  deep-commons/src/main/java/com/stratio/deep/extractor/client/codecs/ResponseDecoder.java 	git commit  deep-commons/src/main/java/com/stratio/deep/extractor/client/codecs/ResponseEncoder.java 	git commit  deep-commons/src/main/java/com/stratio/deep/extractor/response/ComputeResponse.java 	git commit  deep-commons/src/main/java/com/stratio/deep/extractor/response/GetPartitionsResponse.java 	git commit  deep-commons/src/main/java/com/stratio/deep/extractor/response/Response.java 	git commit  deep-commons/src/main/java/com/stratio/deep/rdd/DeepGenericHadoopRDD.java 	git commit  deep-commons/src/main/java/com/stratio/deep/rdd/DeepJavaRDD.java 	git commit  deep-commons/src/main/java/com/stratio/deep/rdd/DeepRDD.java 	git commit  deep-commons/src/main/java/com/stratio/deep/rdd/IDeepHadoopRDD.java

--- deep-commons/src/main/java/com/stratio/deep/context/DeepSparkContext.java
+++ deep-commons/src/main/java/com/stratio/deep/context/DeepSparkContext.java
@@ -15,19 +15,6 @@
 package com.stratio.deep.context;
 
 
-import java.io.Serializable;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Map;
-
-import org.apache.log4j.Logger;
-import org.apache.spark.SparkContext;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.rdd.RDD;
-
-import scala.reflect.ClassTag$;
-
 import com.stratio.deep.config.IDeepJobConfig;
 import com.stratio.deep.entity.Cells;
 import com.stratio.deep.exception.DeepInstantiationException;
@@ -35,123 +22,131 @@ import com.stratio.deep.rdd.DeepGenericHadoopRDD;
 import com.stratio.deep.rdd.DeepJavaRDD;
 import com.stratio.deep.rdd.DeepRDD;
 import com.stratio.deep.rdd.IDeepHadoopRDD;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.rdd.RDD;
+import scala.reflect.ClassTag$;
+
+import java.io.Serializable;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 
 /**
  * Entry point to the Cassandra-aware Spark context.
- * 
+ *
  * @author Luca Rosellini <luca@stratio.com>
  */
 public class DeepSparkContext extends JavaSparkContext implements Serializable {
 
-  private static final Logger LOG = Logger.getLogger(DeepSparkContext.class);
+    private static final Logger LOG = Logger.getLogger(DeepSparkContext.class);
 
-  /**
-   * Overridden superclass constructor.
-   * 
-   * @param sc an already created spark context.
-   */
-  public DeepSparkContext(SparkContext sc) {
-    super(sc);
-  }
-
-  /**
-   * Overridden superclass constructor.
-   * 
-   * @param master the url of the master node.
-   * @param appName the name of the application.
-   */
-  public DeepSparkContext(String master, String appName) {
-    super(master, appName);
-  }
-
-  /**
-   * Overridden superclass constructor.
-   * 
-   * @param master the url of the master node.
-   * @param appName the name of the application.
-   * @param sparkHome the spark home folder.
-   * @param jarFile the jar file to serialize and send to all the cluster nodes.
-   */
-  public DeepSparkContext(String master, String appName, String sparkHome, String jarFile) {
-    super(master, appName, sparkHome, jarFile);
-  }
-
-  /**
-   * Overridden superclass constructor.
-   * 
-   * @param master the url of the master node.
-   * @param appName the name of the application.
-   * @param sparkHome the spark home folder.
-   * @param jars the jar file(s) to serialize and send to all the cluster nodes.
-   */
-  public DeepSparkContext(String master, String appName, String sparkHome, String[] jars) {
-    super(master, appName, sparkHome, jars);
-  }
-
-
-
-  /**
-   * Overridden superclass constructor.
-   * 
-   * @param master the url of the master node.
-   * @param appName the name of the application.
-   * @param sparkHome the spark home folder.
-   * @param jars the jar file(s) to serialize and send to all the cluster nodes.
-   * @param environment a map of environment variables.
-   */
-  public DeepSparkContext(String master, String appName, String sparkHome, String[] jars,
-      Map<String, String> environment) {
-    super(master, appName, sparkHome, jars, environment);
-  }
-
-
-  /**
-   * 
-   * @param deepJobConfig
-   * @param <T>
-   * @return
-   */
-  public <T> RDD<T> createRDD(final IDeepJobConfig deepJobConfig) {
-    try {
-
-      Class rdd = deepJobConfig.getRDDClass();
-      final Constructor c = rdd.getConstructor();
-      if (deepJobConfig.getInputFormatClass() != null) {
-        return new DeepGenericHadoopRDD(this.sc(), deepJobConfig, deepJobConfig.getInputFormatClass(),
-            (IDeepHadoopRDD) c.newInstance(), ClassTag$.MODULE$.<Cells>apply(deepJobConfig
-                .getEntityClass()), ClassTag$.MODULE$.Any(), ClassTag$.MODULE$.Any());
-      } else {
-
-        return new DeepRDD<T>(this.sc(), deepJobConfig);
-      }
-
-    } catch (InstantiationException | NoSuchMethodException | InvocationTargetException
-        | IllegalAccessException e) {
-      LOG.error("impossible to instance IDeepJobConfig, check configuration");
-      throw new DeepInstantiationException(e.getMessage());
+    /**
+     * Overridden superclass constructor.
+     *
+     * @param sc an already created spark context.
+     */
+    public DeepSparkContext(SparkContext sc) {
+        super(sc);
     }
 
-  }
-
-  /**
-   * 
-   * @param deepJobConfig
-   * @param <T>
-   * @param <S>
-   * @return
-   */
-  public <T, S extends IDeepJobConfig<?, ?>> JavaRDD<T> createJavaRDD(
-      IDeepJobConfig<T, S> deepJobConfig) {
-    return new DeepJavaRDD((DeepRDD<T>) createRDD(deepJobConfig));
-  }
-
-
-  public <T, S extends IDeepJobConfig<?, ?>> void saveRDD(RDD<T> rdd,
-      IDeepJobConfig<T, S> deepJobConfig) {
-    try {
-      deepJobConfig.getSaveMethod().invoke(null, rdd, deepJobConfig);
-    } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
-      throw new DeepInstantiationException(e.getMessage());
+    /**
+     * Overridden superclass constructor.
+     *
+     * @param master  the url of the master node.
+     * @param appName the name of the application.
+     */
+    public DeepSparkContext(String master, String appName) {
+        super(master, appName);
     }
-  }
+
+    /**
+     * Overridden superclass constructor.
+     *
+     * @param master    the url of the master node.
+     * @param appName   the name of the application.
+     * @param sparkHome the spark home folder.
+     * @param jarFile   the jar file to serialize and send to all the cluster nodes.
+     */
+    public DeepSparkContext(String master, String appName, String sparkHome, String jarFile) {
+        super(master, appName, sparkHome, jarFile);
+    }
+
+    /**
+     * Overridden superclass constructor.
+     *
+     * @param master    the url of the master node.
+     * @param appName   the name of the application.
+     * @param sparkHome the spark home folder.
+     * @param jars      the jar file(s) to serialize and send to all the cluster nodes.
+     */
+    public DeepSparkContext(String master, String appName, String sparkHome, String[] jars) {
+        super(master, appName, sparkHome, jars);
+    }
+
+
+    /**
+     * Overridden superclass constructor.
+     *
+     * @param master      the url of the master node.
+     * @param appName     the name of the application.
+     * @param sparkHome   the spark home folder.
+     * @param jars        the jar file(s) to serialize and send to all the cluster nodes.
+     * @param environment a map of environment variables.
+     */
+    public DeepSparkContext(String master, String appName, String sparkHome, String[] jars,
+                            Map<String, String> environment) {
+        super(master, appName, sparkHome, jars, environment);
+    }
+
+
+    /**
+     * @param deepJobConfig
+     * @param <T>
+     * @return
+     */
+    public <T> RDD<T> createRDD(final IDeepJobConfig deepJobConfig) {
+        try {
+
+            Class rdd = deepJobConfig.getRDDClass();
+            final Constructor c = rdd.getConstructor();
+            if (deepJobConfig.getInputFormatClass() != null) {
+                return new DeepGenericHadoopRDD(this.sc(), deepJobConfig, deepJobConfig.getInputFormatClass(),
+                        (IDeepHadoopRDD) c.newInstance(), ClassTag$.MODULE$.<Cells>apply(deepJobConfig
+                        .getEntityClass()), ClassTag$.MODULE$.Any(), ClassTag$.MODULE$.Any());
+            } else {
+
+                return new DeepRDD<T>(this.sc(), deepJobConfig);
+            }
+
+        } catch (InstantiationException | NoSuchMethodException | InvocationTargetException
+                | IllegalAccessException e) {
+            LOG.error("impossible to instance IDeepJobConfig, check configuration");
+            throw new DeepInstantiationException(e.getMessage());
+        }
+
+    }
+
+    /**
+     * @param deepJobConfig
+     * @param <T>
+     * @param <S>
+     * @return
+     */
+    public <T, S extends IDeepJobConfig<?, ?>> JavaRDD<T> createJavaRDD(
+            IDeepJobConfig<T, S> deepJobConfig) {
+        return new DeepJavaRDD((DeepRDD<T>) createRDD(deepJobConfig));
+    }
+
+
+    public <T, S extends IDeepJobConfig<?, ?>> void saveRDD(RDD<T> rdd,
+                                                            IDeepJobConfig<T, S> deepJobConfig) {
+        try {
+            deepJobConfig.getSaveMethod().invoke(null, rdd, deepJobConfig);
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            throw new DeepInstantiationException(e.getMessage());
+        }
+    }
 }
