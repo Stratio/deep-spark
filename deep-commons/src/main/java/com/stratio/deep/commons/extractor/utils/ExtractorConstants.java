a506ce48fc4aaf5f0092dc844f1e08c5968a69da
David Gomez <dgomezperez@gmail.com>
2014-11-25 12:35:27 +0100

    New ExtractorConstans

--- deep-commons/src/main/java/com/stratio/deep/commons/extractor/utils/ExtractorConstants.java
+++ deep-commons/src/main/java/com/stratio/deep/commons/extractor/utils/ExtractorConstants.java
@@ -3,60 +3,169 @@ package com.stratio.deep.commons.extractor.utils;
 /**
  * Created by dgomez on 25/08/14.
  */
-public class ExtractorConstants {
+public interface ExtractorConstants {
 
-    public static final String PASSWORD = "password";
+    /**
+     * The PASSWORD.
+     */
+    String PASSWORD = "password";
 
+    /**
+     * The CATALOG.
+     */
+    String CATALOG = "catalog";
+    /**
+     * The KEYSPACE.
+     */
+    String KEYSPACE = CATALOG;
+    /**
+     * The DATABASE.
+     */
+    String DATABASE = KEYSPACE;
+    /**
+     * The INDEX.
+     */
+    String INDEX = CATALOG;
+    /**
+     * The TABLE.
+     */
+    String TABLE = "table";
+    /**
+     * The COLLECTION.
+     */
+    String COLLECTION = TABLE;
+    /**
+     * The TYPE.
+     */
+    String TYPE = TABLE;
+    /**
+     * The NAMESPACE.
+     */
+    String NAMESPACE = CATALOG;
+    /**
+     * The SET.
+     */
+    String SET = TABLE;
+    /**
+     * The HOST.
+     */
+    String HOST = "host";
+    /**
+     * The PORT.
+     */
+    String PORT = "Port";
+    /**
+     * The INPUT _ cOLUMNS.
+     */
+    String INPUT_COLUMNS = "inputColumns";
+    /**
+     * The USERNAME.
+     */
+    String USERNAME = "user";
+    /**
+     * The PAGE _ sIZE.
+     */
+    String PAGE_SIZE = "page";
+    /**
+     * The SESSION.
+     */
+    String SESSION = "session";
+    /**
+     * The PORT 2.
+     */
+    String PORT2 = "port2";
+    /**
+     * The CQLPORT.
+     */
+    String CQLPORT = PORT;
+    /**
+     * The RPCPORT.
+     */
+    String RPCPORT = PORT2;
+    /**
+     * The COLUMN _ fAMILY.
+     */
+    String COLUMN_FAMILY = "columnFamily";
+    /**
+     * The BISECT _ fACTOR.
+     */
+    String BISECT_FACTOR = "bisecFactor";
+    /**
+     * The CREATE _ ON _ WRITE.
+     */
+    String CREATE_ON_WRITE = "createOnWrite";
+    /**
+     * The BATCHSIZE.
+     */
+    String BATCHSIZE = "batchSize";
+    /**
+     * The READ _ CONSISTENCY _ lEVEL.
+     */
+    String READ_CONSISTENCY_LEVEL = "readConsistencyLevel";
+    /**
+     * The WRITE _ CONSISTENCY _ lEVEL.
+     */
+    String WRITE_CONSISTENCY_LEVEL = "writeConsistencyLevel";
 
-    public static final String CATALOG = "catalog";
-    public static final String KEYSPACE = CATALOG;
-    public static final String DATABASE = KEYSPACE;
-    public static final String INDEX = CATALOG;
-    public static final String TABLE    = "table";
-    public static final String COLLECTION = TABLE;
-    public static final String TYPE = TABLE;
-    public static final String HOST     = "host";
-    public static final String PORT     = "port";
-    public static final String HOSTS    = "Hosts";
-    public static final String PORTS    = "Port";
+    /**
+     * The REPLICA _ SET.
+     */
+    String REPLICA_SET = "replicaSet";
 
-    public static final String INNERCLASS = "implClass";
+    /**
+     * The READ _ PREFERENCE.
+     */
+    String READ_PREFERENCE = "readPreference";
 
-    public static final String INPUT_COLUMNS = "inputColumns";
-    public static final String USERNAME  ="user";
-    public static final String PAGE_SIZE = "page";
-    public static final String SESSION  = "session";
-    public static final String PORT2  = "port2";
-    public static final String CQLPORT  = PORT2;
-    public static final String RPCPORT  = PORT;
-    public static final String COLUMN_FAMILY  = "columnFamily";
-    public static final String BISECT_FACTOR  = "bisecFactor";
-    public static final String CREATE_ON_WRITE  = "createOnWrite";
-    public static final String BATCHSIZE  = "batchSize";
-    public static final String READ_CONSISTENCY_LEVEL = "readConsistencyLevel";
-    public static final String WRITE_CONSISTENCY_LEVEL = "writeConsistencyLevel";
+    /**
+     * The SORT.
+     */
+    String SORT = "sort";
 
-    public static final String REPLICA_SET = "replicaSet";
+    /**
+     * The FILTER _ FIELD.
+     */
+    String FILTER_FIELD = "filterField";
 
-    public static final String READ_PREFERENCE = "readPreference";
+    /**
+     * The FILTER _ QUERY.
+     */
+    String FILTER_QUERY = "filterQuery";
 
-    public static final String SORT = "sort";
+    /**
+     * The INPUT _ kEY.
+     */
+    String INPUT_KEY = "inputKey";
 
-    public static final String FILTER_FIELD = "filterField";
+    /**
+     * The IGNORE _ ID _ FIELD.
+     */
+    String IGNORE_ID_FIELD = "ignoreIdField";
 
-    public static final String FILTER_QUERY = "filterQuery";
+    /**
+     * The USE _ sHARD.
+     */
+    String USE_SHARD = "useShard";
 
-    public static final String INPUT_KEY = "inputKey";
+    /**
+     * The USE _ SPLITS.
+     */
+    String USE_SPLITS = "useSplit";
 
-    public static final String IGNORE_ID_FIELD = "ignoreIdField";
+    /**
+     * The USE _ CHUNKS.
+     */
+    String USE_CHUNKS = "useChunk";
 
-    public static final String USE_SHARD = "useShard";
+    /**
+     * The SPLIT _ SIZE.
+     */
+    String SPLIT_SIZE = "splitSize";
 
-    public static final String USE_SPLITS = "useSplit";
-
-    public static final String USE_CHUNKS = "useChunk";
-
-    public static final String SPLIT_SIZE = "splitSize";
+    /**
+     * The EQUALS _ IN _ FILTER.
+     */
+    String EQUALS_IN_FILTER = "equalsInFilter";
 
     public static final String HDFS  = "hdfs";
     public static final String HDFS_TYPE  = "hdfsType";
@@ -67,5 +176,4 @@ public class ExtractorConstants {
     public static final String HDFS_FILE_PATH = "path";
     public static final String HDFS_SCHEMA = "schemaMap";
     public static final String TYPE_CONSTANT = "type";
-
 }
