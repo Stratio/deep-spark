fd9d24b956aae4ddf1fadf1fa216e29cafb4c56c
Mario Muñoz <mariomgal@gmail.com>
2014-12-16 16:40:41 +0100

    Oracle reading example.

--- deep-examples/src/main/java/com/stratio/deep/examples/java/ReadingCellsFromJdbc.java
+++ deep-examples/src/main/java/com/stratio/deep/examples/java/ReadingCellsFromJdbc.java
@@ -1,7 +1,20 @@
+/*
+ * Copyright 2014, Stratio.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.stratio.deep.examples.java;
 
-import com.stratio.deep.aerospike.config.AerospikeConfigFactory;
-import com.stratio.deep.aerospike.config.AerospikeDeepJobConfig;
 import com.stratio.deep.core.context.DeepSparkContext;
 import com.stratio.deep.jdbc.config.JdbcConfigFactory;
 import com.stratio.deep.jdbc.config.JdbcDeepJobConfig;
@@ -10,7 +23,7 @@ import org.apache.log4j.Logger;
 import org.apache.spark.rdd.RDD;
 
 /**
- * Created by mariomgal on 10/12/14.
+ * Usage of JdbcExtractor for Reading Cells with Oracle.
  */
 public class ReadingCellsFromJdbc {
 
